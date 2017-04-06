9d75155056e4abe5e924fc17a95af1b46d53d4d6
Aldey Wahyu Putra <dragonfirecmv@hotmail.com>
2017-04-06 17:47:18 +0700

    Bug Fix: 1. Fix error line to str 2. remove unnecessary newLine

--- learning/read_lineByLine_toList.py
+++ learning/read_lineByLine_toList.py
@@ -5,18 +5,21 @@ __name__ = "Read line by line and store it to List"
 __version__ = '0.1'
 
 def readLineByLine(filepath):
-    koleksi_nama = []
+    koleksi_nama = list()
 
     path = Path(filepath)
     if path.is_file():
         print("File {0} ditemukan!".format(filepath))
         with open(filepath) as f:
             for line in f:
-                koleksi_nama = list.append(line)
-
+                koleksi_nama.append(line)
+                koleksi_nama = [x.strip() for x in koleksi_nama]
         if type(koleksi_nama) is list:
-            print("Type is a list")
+            print("Selamat! Tipe objek ini ialah List!")
             return koleksi_nama
+            '''for test in koleksi_nama:
+                print(test)'''
+                
         else:
             return "ERR_01"
     else:
