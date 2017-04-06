49228c1728b69d392c3ad9cdbec3e0a5c31d76a1
Eufrat Tsaqib <eutsat@gmail.com>
2017-04-06 15:43:45 +0700

    tambah naive

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# gender-ml
-Experimental ML on gender classification
+# App Engine Standard Flask Tutorial App
+
+This sample shows how to use [Flask](http://flask.pocoo.org/) to handle
+requests, forms, templates, and static files on Google App Engine Standard.
+
+Before running or deploying this application, install the dependencies using
+[pip](http://pip.readthedocs.io/en/stable/):
+
+    pip install -t lib -r requirements.txt
+
+For more information, see the [App Engine Standard README](../../README.md)
---
+++ learning/naive1.py
@@ -0,0 +1,47 @@
+from nltk.corpus import names
+import random
+
+# ambil kata pertama dari kalimat
+def get_first_word(words):
+    words_list = words.split()
+    if len(words_list) > 1 :
+        # kalau lebih besar dari satu kata dalam kalimat, return kata pertama
+        return words_list[0]
+    else:
+        # kalau hanya satu kata dalam kalimat, return kata itu
+        return words
+
+# buat feature berdasarkan huruf terakhir dari
+def gender_features_last(word):
+    word = get_first_word(word) # ambil kata pertama
+    return {'last_letter': word[-1]} # return huruf terakhir
+
+def gender_features_last(word):
+    word = get_first_word(word) # ambil kata pertama
+    return {'first_letter': word[0]} # return huruf pertama
+
+def gender_features_last_two(word):
+    word = get_first_word(word) # ambil kata pertama
+    return {'last_two': word[-1] + word[-2]} # return huruf 2 kata terakhir
+
+labeled_names = (
+    [(name, 'male') for name in names.words('male.txt')] + 
+    [(name, 'female') for name in names.words('female.txt')]
+)
+
+random.shuffle(labeled_names)
+
+featuresets = [(gender_features_last(n), gender) for (n, gender) in labeled_names]
+
+train_set = featuresets[500:]
+test_set = featuresets[:500]
+
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+
+print "accuracy is: "
+print (nltk.classify.accuracy(classifier, test_set))
+
+classifier.show_most_informative_features()
+
+
+
