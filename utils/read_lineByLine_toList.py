2dc373ff9dbad79378cecd22e465db93c00116af
Eufrat Tsaqib <eutsat@gmail.com>
2017-04-07 16:51:41 +0700

    remove comments

--- utils/read_lineByLine_toList.py
+++ utils/read_lineByLine_toList.py
@@ -1,33 +1,18 @@
 from pathlib import Path
-# v0.0.1
-# using Python 3.5
-
-__name__ = '__read_lineByLine__'
-__version__ = '0.1'
 
 def readLineByLine(filepath):
-    '''
-    [Function]
-    Desc: Read Line by line to array from file
-    Version = 0.1
-    (c) Aldey W Putra
-    '''
     koleksi_nama = list()
-
     path = Path(filepath)
     if path.is_file():
-        print("File {0} ditemukan!".format(filepath))
+        print("File {0} found".format(filepath))
         with open(filepath) as f:
             for line in f:
                 koleksi_nama.append(line)
                 koleksi_nama = [x.strip() for x in koleksi_nama]
         if type(koleksi_nama) is list:
-            print("Selamat! Tipe objek ini ialah List!")
             return koleksi_nama
-            '''for test in koleksi_nama:
-                print(test)'''
-                
         else:
+            print("Data type is not list")
             return "ERR_01"
     else:
-        print("Maaf, file tersebut tidak ditemukan!")
+        print("File is not found.")
