50401c46e4876ef9b151b6f3bd5901ef010c13bc
Aldey Wahyu Putra <dragonfirecmv@hotmail.com>
2017-04-07 18:09:25 +0700

    1. ren "_sentence_" -> "_listobj_" 2. refine func

--- learning/utils/split_and_tuple_it.py
+++ learning/utils/split_and_tuple_it.py
@@ -1,5 +1,4 @@
 import random
 
-def split_from_sentence_and_tuple_it(sentence, splitchar=" "):
-    word_array = sentence.split(splitchar)
-    
+def split_from_listobj_and_tuple_it(listobj, attr):
+    labeled_word = [(listobj, attr) for word in listobj]
