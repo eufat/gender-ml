024d251bbff0e80e29d31ea150b139abdf57f094
Aldey Wahyu Putra <dragonfirecmv@hotmail.com>
2017-04-07 20:42:20 +0700

    add func count_vowels and count_consonants

---
+++ learning/utils/find_consonants.py
@@ -0,0 +1,10 @@
+
+def count_vowels(word):
+    vowels = "aeiou"
+    ch = word.lower()
+    return sum(letter in vowels for letter in ch)
+
+def count_consonants(word):
+    consonants = "bcdfghjklmnpqrstvwxyz"
+    ch = word.lower()
+    return sum(letter in consonants for letter in ch)
