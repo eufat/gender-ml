6f9d1aea623d66221f1e4ca4c8f079dbbcfcbf51
Aldey Wahyu Putra <dragonfirecmv@hotmail.com>
2017-04-10 17:13:06 +0700

    optimize oop

--- src_oop/main.py
+++ src_oop/main.py
@@ -1,9 +1,9 @@
-import libs.nltk_gender_ml
+from libs.nltk_gender_ml import *
 class Program:
     
     def main(self, *args):
-        genderml = libs.nltk_gender_ml.NLTKGenderML('datasets/male.csv', 'datasets/female.csv')
+        genderml = NLTKGenderML('datasets/male.csv', 'datasets/female.csv')
         genderml.show_maintenance()
-        genderml.test_person_gender_by_name('Yuma Fahmi Alama')
+        genderml.test_person_gender_by_name('Yuma Fahmi Alam')
 
 Program().main()
